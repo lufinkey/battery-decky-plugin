f69396b1e3da24af76175cb57aad76efa58026d6
Luis Finke <luisfinke@gmail.com>
2023-01-07 15:27:58 -0500

    empty test script

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import sys
 PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(PLUGIN_DIR+"/py_modules")
-sys.path.append(PLUGIN_DIR+"/backend")
-
 import logging
+
+sys.path.append(PLUGIN_DIR+"/backend")
+from upower_monitor import UPowerMonitor
 
 logging.basicConfig(filename="/tmp/template.log",
                     format='[Template] %(asctime)s %(levelname)s %(message)s',
@@ -14,9 +15,12 @@ logger=logging.getLogger()
 logger.setLevel(logging.INFO) # can be changed to logging.DEBUG for debugging issues
 
 class Plugin:
+    monitor: UPowerMonitor = None
+
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         logger.info("Loading Battery Info plugin")
+        
     
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
